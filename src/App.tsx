2d614f17d802a63440fefe2c3a2883d4b95f0b9c
FrancoisGib <francois.gib54@gmail.com>
2024-08-22 17:24:53 +0200

    improved examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useFieldValidator } from "./hooks/useFieldValidator";
 import { useFormValidator } from "./hooks/useFormValidator";
 import { Validator } from "./lib/Validators";
@@ -10,14 +11,20 @@ export default function App() {
     {name: "password", initialValue: "", validationFunctions: [Validator.password]}
   );
 
+  const [showPassword, setShowPassword] = useState(false);
+
   const [nonBlankInput, setInput, isInputValid] = useFieldValidator("", Validator.nonBlank)
 
   return (
     <div className="App">
     <form>
-      <input type="email" name="email" value={form["email"].value} onChange={setForm} />
-      <input type="text" name="name" value={form["name"].value} onChange={setForm} />
-      <input type="text" name="password" value={form["password"].value} onChange={setForm} />
+      Email : <input type="email" name="email" value={form["email"].value} onChange={setForm} />
+      <br/>
+      Name : <input type="text" name="name" value={form["name"].value} onChange={setForm} />
+      <br/>
+      Password : <input type={showPassword ? "text" : "password"} name="password" value={form["password"].value} onChange={setForm} />
+      
+      <input type="button" onClick={ () => setShowPassword(!showPassword) } value="Show password" />
     </form>
 
       <p>Email valid : {form["email"].isValid.toString()}</p>
@@ -25,7 +32,9 @@ export default function App() {
       <p>Password valid : {form["password"].isValid.toString()}</p>
       <p>Form valid : {isValid.toString()}</p>
 
-      <input type="text" name="non-blank" value={nonBlankInput} onChange={setInput} />
+      <br/><br/>
+      Non blank input : <input type="text" name="non-blank" value={nonBlankInput} onChange={setInput} />
+      <p>Non-blank input valid : {isInputValid.toString()}</p>
     </div>
   );
 }
